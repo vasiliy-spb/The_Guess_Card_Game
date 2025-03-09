336c5893f665811c9b3cb166616a7e8cd20ecf4a
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-03-09 19:35:06 +0700

    Made a refactor of MonochromeConsoleRender class

--- src/main/java/org/cheercode/renders/MonochromeConsoleRender.java
+++ src/main/java/org/cheercode/renders/MonochromeConsoleRender.java
@@ -4,28 +4,43 @@ import org.cheercode.cards.Card;
 import org.cheercode.cards.CardRanks;
 
 public class MonochromeConsoleRender extends AbstractConsoleRender {
+    private static final String REPRESENTATION_DELIMITER = " ";
+    private static final String TWO_RANK_REPRESENTATION = "2";
+    private static final String THREE_RANK_REPRESENTATION = "3";
+    private static final String FOUR_RANK_REPRESENTATION = "4";
+    private static final String FIVE_RANK_REPRESENTATION = "5";
+    private static final String SIX_RANK_REPRESENTATION = "6";
+    private static final String SEVEN_RANK_REPRESENTATION = "7";
+    private static final String EIGHT_RANK_REPRESENTATION = "8";
+    private static final String NINE_RANK_REPRESENTATION = "9";
+    private static final String TEN_RANK_REPRESENTATION = "10";
+    private static final String JACK_RANK_REPRESENTATION = "J";
+    private static final String QUEEN_RANK_REPRESENTATION = "Q";
+    private static final String KING_RANK_REPRESENTATION = "K";
+    private static final String ACE_RANK_REPRESENTATION = "A";
+
     @Override
     protected String getRepresentation(Card card) {
         String rankRepresentation = getRankRepresentation(card);
-        return card.suit().getValue() + " " + rankRepresentation;
+        return card.suit().getValue() + REPRESENTATION_DELIMITER + rankRepresentation;
     }
 
     private String getRankRepresentation(Card card) {
         CardRanks rank = card.rank();
         return switch (rank) {
-            case TWO -> "2";
-            case THREE -> "3";
-            case FOUR -> "4";
-            case FIVE -> "5";
-            case SIX -> "6";
-            case SEVEN -> "7";
-            case EIGHT -> "8";
-            case NINE -> "9";
-            case TEN -> "10";
-            case JACK -> "J";
-            case QUEEN -> "Q";
-            case KING -> "K";
-            case ACE -> "A";
+            case TWO -> TWO_RANK_REPRESENTATION;
+            case THREE -> THREE_RANK_REPRESENTATION;
+            case FOUR -> FOUR_RANK_REPRESENTATION;
+            case FIVE -> FIVE_RANK_REPRESENTATION;
+            case SIX -> SIX_RANK_REPRESENTATION;
+            case SEVEN -> SEVEN_RANK_REPRESENTATION;
+            case EIGHT -> EIGHT_RANK_REPRESENTATION;
+            case NINE -> NINE_RANK_REPRESENTATION;
+            case TEN -> TEN_RANK_REPRESENTATION;
+            case JACK -> JACK_RANK_REPRESENTATION;
+            case QUEEN -> QUEEN_RANK_REPRESENTATION;
+            case KING -> KING_RANK_REPRESENTATION;
+            case ACE -> ACE_RANK_REPRESENTATION;
         };
     }
 }
