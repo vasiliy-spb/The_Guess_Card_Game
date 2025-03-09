440323eeca275e9d258dcabf2ea8041391293e8a
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-03-09 19:25:46 +0700

    Made a refactor of GameVariantMenuItem class

--- src/main/java/org/cheercode/menu/items/GameVariantMenuItem.java
+++ src/main/java/org/cheercode/menu/items/GameVariantMenuItem.java
@@ -3,7 +3,6 @@ package org.cheercode.menu.items;
 import org.cheercode.menu.GameVariants;
 
 public class GameVariantMenuItem extends MenuItem<Integer, GameVariants> {
-
     public GameVariantMenuItem(Integer key, String title, GameVariants result) {
         super(key, title, result);
     }
