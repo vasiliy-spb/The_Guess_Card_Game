a0b5fa5057e07cd7c3f135c52170914915465a62
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-03-09 21:38:25 +0700

    Made a refactor of MenuItem class

--- src/main/java/org/cheercode/menu/items/MenuItem.java
+++ src/main/java/org/cheercode/menu/items/MenuItem.java
@@ -1,12 +1,12 @@
 package org.cheercode.menu.items;
 
-public abstract class MenuItem<T, R> {
+public abstract class MenuItem<K, R> {
     private static final String REPRESENTATION_DELIMITER = " — ";
-    private final T key;
+    private final K key;
     private final String title;
     private final R result;
 
-    public MenuItem(T key, String title, R result) {
+    public MenuItem(K key, String title, R result) {
         this.key = key;
         this.title = title;
         this.result = result;
