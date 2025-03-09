4dc0037c49dff81552dfbc810578b551760d3717
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-03-09 19:37:18 +0700

    Made a refactor of CardRankGameResultAnalyzer class

--- src/main/java/org/cheercode/result_analyzers/CardRankGameResultAnalyzer.java
+++ src/main/java/org/cheercode/result_analyzers/CardRankGameResultAnalyzer.java
@@ -5,8 +5,8 @@ import org.cheercode.cards.CardRanks;
 
 public class CardRankGameResultAnalyzer implements GameResultAnalyzer<org.cheercode.cards.CardRanks> {
     @Override
-    public boolean getResult(CardRanks selectedType, Card card) {
+    public boolean getResult(CardRanks selectedRank, Card card) {
         CardRanks cardRank = card.rank();
-        return selectedType.equals(cardRank);
+        return selectedRank.equals(cardRank);
     }
 }
